1221fc0ddd5238c55e3b529713e6b262189643a4
Spence Southard <Spence.Southard@gmail.com>
2016-02-18 21:00:05 -0500

    Reorganized listen method

--- SimpleServerSocket.java
+++ SimpleServerSocket.java
@@ -12,21 +12,20 @@ public class SimpleServerSocket<SimpleRunnableType extends SimpleRunnable> exten
     private ExecutorService executorService;
     private Class<SimpleRunnableType> srt;
 
-    public SimpleServerSocket(int port, Class<SimpleRunnableType> srt) throws IOException{
+    public SimpleServerSocket(int port, Class<SimpleRunnableType> srt) throws IOException {
         super(port);
         int numberOfThreads = 1 + Runtime.getRuntime().availableProcessors();
         executorService = Executors.newFixedThreadPool(numberOfThreads);
         this.srt = srt;
     }
 
-    public SimpleServerSocket(int port, Class<SimpleRunnableType> srt, int threadPoolSize) throws IOException{
+    public SimpleServerSocket(int port, Class<SimpleRunnableType> srt, int threadPoolSize) throws IOException {
         super(port);
         executorService = Executors.newFixedThreadPool(threadPoolSize);
         this.srt = srt;
     }
 
-    private SimpleRunnableType createThread() throws InstantiationException, IllegalAccessException
-    {
+    private SimpleRunnableType createThread() throws InstantiationException, IllegalAccessException {
         return srt.newInstance();
     }
 
@@ -34,22 +33,18 @@ public class SimpleServerSocket<SimpleRunnableType extends SimpleRunnable> exten
         this.verbose = verbose;
     }
 
-    public void listen(){
+    public void listen() throws IOException, IllegalAccessException, InstantiationException{
 
         while(true){
 
             SimpleRunnableType socketThread = null;
 
-            try{
-                Socket socket = this.accept();
-                socket.setSoTimeout(30000);
-                socketThread = createThread();
-                socketThread.setSimpleSocket(socket);
-                if(verbose) System.out.println("Received socket connection.");
-            }
-            catch(Exception se){
-                se.printStackTrace();
-            }
+            Socket socket = this.accept();
+            socket.setSoTimeout(30000);
+            socketThread = createThread();
+            socketThread.setSimpleSocket(socket);
+
+            if(verbose) System.out.println("Received socket connection.");
 
             executorService.execute(socketThread);
 
